fd364c8db3c12d6094ec8ed10ac70012748973c1
Kip Gibson <Kiptong@gmail.com>
2017-06-09 16:03:29 -0700

    Selecting Individual Char. #3

---
+++ main.js
@@ -0,0 +1,14 @@
+var test = document.getElementById('typeTest')
+var text = test.textContent
+var chars = text.split('')
+
+function renderChar(char) {
+  var $char = document.createElement('span')
+  $char.textContent = char
+  return $char
+}
+test.textContent = ''
+for (var i = 0; i < chars.length; i++) {
+  var $span = renderChar(chars[i])
+  test.appendChild($span)
+}
