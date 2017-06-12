49497f2e9d9945c377e01d022b232031190c93f0
Kip Gibson <Kiptong@gmail.com>
2017-06-12 10:46:45 -0700

    write javascript to view bold letter

--- main.js
+++ main.js
@@ -2,13 +2,21 @@ var test = document.getElementById('typeTest')
 var text = test.textContent
 var chars = text.split('')
 
+
+
+
+
+
 function renderChar(char) {
   var $char = document.createElement('span')
   $char.textContent = char
   return $char
 }
 test.textContent = ''
+
 for (var i = 0; i < chars.length; i++) {
   var $span = renderChar(chars[i])
   test.appendChild($span)
 }
+var $current = document.querySelector('span')
+$current.classList.add('current')
